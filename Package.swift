2ad68390e76455a094ce753afc452487573d52c1
Alexey Nenastev <a-nenastev@mail.ru>
2024-06-19 14:40:35 +0200

    Remove unnecessary comments from Package.swift
    - Cleaned up unnecessary comments in the Package.swift file to improve readability and maintainability.

--- Package.swift
+++ Package.swift
@@ -19,8 +19,6 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-http-types.git", from: "1.1.0")
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "URLConfig",
       dependencies: [
